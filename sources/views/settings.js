14b0904001061d2b2f39a343cf53f228d7b4ef96
AntonKosmovskyXB <akosmovsky@xbsoftware.by>
2021-04-16 04:48:24 +0300

    add contacts and settings part

---
+++ sources/views/settings.js
@@ -0,0 +1,19 @@
+import {JetView} from "webix-jet";
+
+export default class SettingsView extends JetView{
+	config(){
+        return {
+            rows:[
+                {
+                    view: "segmented",
+                    width: 500,
+                    options: [
+                        { id:"en", value:"en" },
+                        { id:"ru", value:"ru"}
+                    ]
+                },
+                {}
+            ]
+        }
+	}
+}
---
+++ sources/views/top.js
@@ -0,0 +1,25 @@
+import {JetView} from "webix-jet";
+
+export default class TopView extends JetView {
+	config(){
+		return {
+			rows:[
+				{ type:"space", cols:[
+					{ 
+						view:"menu", 
+						id:"top:menu", 
+						width:200, 
+						layout: "y",
+						select:true,
+						data:[
+							{id: "contacts", value: "Contacts", href: "#!/top/contacts"},
+							{id: "data", value: "Data", href: "#!/top/datatable"},
+							{id: "settings", value: "Settings", href: "#!/top/settings"}
+						]
+					}, 
+					{ $subview:true }
+				]},
+			]
+		};
+	}
+}
